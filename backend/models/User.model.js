541ac6e8a523ce4cc2df03313ec535a38157bb6d
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-06-18 22:01:18 +0530

    User and Problem Schema created

---
+++ backend/models/User.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+        ,
+        unique: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    eloRating: {
+        type: Number,
+        default: 1200
+    },
+    isTop12: {
+        type: Boolean,
+        default: false
+    },
+    badges: [{
+        type: String
+    }],
+    matchHistory: [{
+        opponent: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        result: {
+            type: String,
+            enum: ["win", "loss", "draw"]
+        },
+        ratingChange: Number
+    }]
+}, {timestamps: true});
+
+export default mongoose.model("User", userSchema);
